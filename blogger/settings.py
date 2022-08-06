666b00d4c55a509275fa3267a3e9f73e385b987f
udonwajnr <udonwajnr10@gmail.com>
2022-08-06 20:00:30 +0100

    first commit

--- blogger/settings.py
+++ blogger/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-o(a(921sv2)p1&hd@rr52s8zp6qw%utn9&te@29o5p64*xpax9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
